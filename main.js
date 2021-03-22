2d95b9ba334147dccb3891ddf540ce05e2c937b2
Sonu kumar kushwaha <flyingsonu736@gmail.com>
2021-03-22 19:09:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let color = "#";
 const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f']
 const wish = document.querySelector('.wish');
 
-setInterval(getColor, 3000);
+setInterval(getColor, 5000);
 
 function getColor() {
     color = "#";
